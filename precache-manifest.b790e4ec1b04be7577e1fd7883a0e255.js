d81fcd0352889334e3bad337188d970d9ef495f0
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-05-28 16:23:11 +0200

    Updates

---
+++ precache-manifest.b790e4ec1b04be7577e1fd7883a0e255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e1dd09a6ded79058dce625955f89ba",
+    "url": "/laurent-game/index.html"
+  },
+  {
+    "revision": "2c3206d0367fb49f9536",
+    "url": "/laurent-game/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "15a171f8e828e69812fb",
+    "url": "/laurent-game/static/js/2.521cd4ab.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/laurent-game/static/js/2.521cd4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3206d0367fb49f9536",
+    "url": "/laurent-game/static/js/main.de1d4161.chunk.js"
+  },
+  {
+    "revision": "0cf7d96619bb9345cec6",
+    "url": "/laurent-game/static/js/runtime-main.974ab5ec.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/laurent-game/precache-manifest.b790e4ec1b04be7577e1fd7883a0e255.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laurent-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
